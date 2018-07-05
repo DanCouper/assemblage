9a81f5ce4ba554cd14160b1998cf05d7babe93ef
Daniel Couper <danielcouper81@googlemail.com>
2018-07-05 09:52:16 +0100

    Add to documentation

--- lib/assemblage/multimedia/multimedia.ex
+++ lib/assemblage/multimedia/multimedia.ex
@@ -1,15 +1,23 @@
 defmodule Assemblage.Multimedia do
   @moduledoc """
   The Multimedia context.
+
+  Tied into the Accounts context to associate multimedia
+  items with users.
+
+  TODO Currently only images.
+  TODO Will also need a posts context to associate with -
+  multimedia items belong to both users and to posts.
   """
 
   import Ecto.Query, warn: false
-  alias Assemblage.Accounts
+  alias Assemblage.Accounts.User
   alias Assemblage.Repo
   alias Assemblage.Multimedia.Image
 
   @doc """
-  Returns the list of images.
+  Returns the list of all images.
+  REVIEW: not sustainable in the case of there being a huge number of images?
 
   ## Examples
 
@@ -23,7 +31,16 @@ defmodule Assemblage.Multimedia do
     |> preload_user()
   end
 
-  def list_user_images(%Accounts.User{} = user) do
+  @doc """
+  Returns the list of all images belonging to a user.
+
+  ## Examples
+
+      iex> list_images(%User{})
+      [%Image{}, ...]
+
+  """
+  def list_user_images(%User{} = user) do
     Image
     |> user_images_query(user)
     |> Repo.all()
@@ -31,7 +48,7 @@ defmodule Assemblage.Multimedia do
   end
 
   @doc """
-  Gets a single image.
+  Given an image ID, gets an image.
 
   Raises `Ecto.NoResultsError` if the Image does not exist.
 
@@ -46,26 +63,40 @@ defmodule Assemblage.Multimedia do
   """
   def get_image!(id), do: preload_user(Repo.get!(Image, id))
 
+  @doc """
+  Given a user and an image ID, gets an image belonging to that user.
 
-  def get_user_image!(%Accounts.User{} = user, id) do
+  Raises `Ecto.NoResultsError` if the Image does not exist.
+
+  ## Examples
+
+      iex> get_image!(123)
+      %Image{}
+
+      iex> get_image!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user_image!(%User{} = user, id) do
     from(i in Image, where: i.id == ^id)
     |> user_images_query(user)
     |> Repo.one!()
     |> preload_user()
   end
+
   @doc """
-  Creates a image.
+  Given a user and an image, creates a image for that user.
 
   ## Examples
 
-      iex> create_image(%{field: value})
+      iex> create_image(%User{}, %{field: value})
       {:ok, %Image{}}
 
-      iex> create_image(%{field: bad_value})
+      iex> create_image(%User{}, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_image(%Accounts.User{} = user, attrs \\ %{}) do
+  def create_image(%User{} = user, attrs \\ %{}) do
     %Image{}
     |> Image.changeset(attrs)
     |> put_user(user)
@@ -111,25 +142,29 @@ defmodule Assemblage.Multimedia do
 
   ## Examples
 
-      iex> change_image(image)
+      iex> change_image(user, image)
       %Ecto.Changeset{source: %Image{}}
 
   """
-  def change_image(%Accounts.User{} = user, %Image{} = image) do
+  def change_image(%User{} = user, %Image{} = image) do
     image
     |> Image.changeset(%{})
     |> put_user(user)
   end
 
+  # Preloads the association for users of images.
   defp preload_user(image_or_images) do
     Repo.preload(image_or_images, :user)
   end
 
+  # Helper to ensure applied changes to images
+  # also update that specific user.
   defp put_user(changeset, user) do
     Ecto.Changeset.put_assoc(changeset, :user, user)
   end
 
-  defp user_images_query(query, %Accounts.User{id: user_id}) do
+  # Ensure images returned belong to a specific user.
+  defp user_images_query(query, %User{id: user_id}) do
     from(i in query, where: i.user_id == ^user_id)
   end
 end
