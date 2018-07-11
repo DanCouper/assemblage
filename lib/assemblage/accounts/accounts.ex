87e7056f3c755f419577a823aa39b801b140b977
Daniel Couper <danielcouper81@googlemail.com>
2018-07-11 11:36:58 +0100

    Specs and fixme notes for  the Accounts context

--- lib/assemblage/accounts/accounts.ex
+++ lib/assemblage/accounts/accounts.ex
@@ -33,6 +33,7 @@ defmodule Assemblage.Accounts do
   @doc """
   Registers (creates) a user
   """
+  @spec register_user(map()) :: {:ok, User.t()} | {:error, String.t()}
   def register_user(attrs \\ %{}) do
     %User{}
     |> User.registration_changeset(attrs)
@@ -43,6 +44,7 @@ defmodule Assemblage.Accounts do
   Update info held on the User struct itself, for example
   the name field.
   """
+  @spec update_user_info(User.t(), map()) :: {:ok, User.t()} | {:error, String.t()}
   def update_user_info(user, params) do
     if Map.has_key?(params, :credential) do
       # Note that any attempt to update credentials via this function
@@ -59,6 +61,7 @@ defmodule Assemblage.Accounts do
   Given a user, update the email held on the associated
   Credential struct. To do so, a password must be given.
   """
+  @spec update_user_email(User.t(), String.t(), String.t()) :: {:ok, User.t()} | {:error, String.t()}
   def update_user_email(user, new_email, password) do
     with {:ok, user} <- check_password(user, password) do
       user
@@ -71,6 +74,7 @@ defmodule Assemblage.Accounts do
   Given a user, update the password held on the associated
   Credential struct. To do so, the current password must be given.
   """
+  @spec update_user_password(User.t(), String.t(), String.t()) :: {:ok, User.t()} | {:error, String.t()}
   def update_user_password(user, new_password, current_password) do
     with {:ok, user} <- check_password(user, current_password) do
       user
@@ -88,17 +92,42 @@ defmodule Assemblage.Accounts do
      notification should be sent to force a password reset.
   5. this is quite complex: a notification system needs to be in place first.
   """
+  @spec delete_user(User.t()) :: {:ok, User.t()} | {:error, String.t()}
   def delete_user(user) do
     Repo.delete(user)
   end
 
   @doc """
   Get a User based on the email stored in their linked credentials.
+
+  FIXME spec is incorrect: Need to return correct error tuples if the lookup fails
   """
+  # @spec get_user_by_email(String.t()) :: {:ok, User.t()} | {:error, String.t()}
   def get_user_by_email(email) do
+    # FIXME deal with lookup failing, will just get `nil` as things stand
     from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
     |> Repo.one()
     |> Repo.preload(:credential)
+  end
+
+  @doc """
+  Get a User based on their auth token.
+
+  FIXME spec is incorrect: Need to return correct error tuples if the lookup fails
+  """
+  # @spec get_user_by_token(String.t()) :: {:ok, User.t()} | {:error, String.t()}
+  def get_user_by_token(token) do
+    case Authenticator.verify_token(token) do
+      {:ok, _} ->
+        # FIXME deal with lookup failing, will just get `nil` as things stand
+        from(u in User, join: c in assoc(u, :auth_token), where: c.token == ^token)
+        |> Repo.one()
+        |> Repo.preload(:auth_token)
+
+      {:error, reason} ->
+        # FIXME just stringify the reason for now
+        {:error, to_string(reason)}
+    end
   end
 
 
@@ -119,10 +148,14 @@ defmodule Assemblage.Accounts do
 
   @doc """
   TODO as above
+  TODO this is `authorized?`, not `signed_in?`. `signed_in` is not really a
+  valid concept (semantically) in this case, once a user has a token, then
+  that's the only thing they need.
   """
+  @spec signed_in?(User.t()) :: boolean()
   def signed_in?(user) do
     token_assoc = user |> Ecto.assoc(:auth_token) |> Repo.one()
-    IO.inspect token_assoc
+
     case token_assoc do
       %{token: nil} -> false
       %{revoked: true} -> false
@@ -131,13 +164,24 @@ defmodule Assemblage.Accounts do
     end
   end
 
+  @doc """
+  TODO this is deletion of a token, not strictly speaking signing out. It will
+  work as a signing out mechanism just fine, but need to
+      a. be a bit more consistent w/r/t return values,
+      b. look at updating via the user (assoc with AuthToken) - currently this will
+        just blow away the AuthToken struct without any reference to the User it belongs to.
+  """
+  @spec sign_out(String.t()) :: {:ok, AuthToken.t()} | {:error, String.t()}
   def sign_out(token) do
     AuthToken
     |> Repo.get_by(%{token: token})
     |> Repo.delete()
   end
 
+  @spec check_password(User.t(), String.t()) :: {:ok, User.t()} | {:error, String.t()}
   def check_password(%User{credential: %{password_hash: password_hash}} = user, password) do
+    # Because the credentials are not stored directly ono the User, Comeonin's handy
+    # `check_pass` function, which does exactly the following, will not work
     case Comeonin.Bcrypt.checkpw(password, password_hash) do
       true -> { :ok, user }
       false -> { :error, "Authentication failed for operation" }
